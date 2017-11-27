f558228a4c857086c4ad50be7ddc3b032f6ed4d2
hadarsabag <hadarsbg@gmail.com>
2017-11-27 21:21:22 +0200

    memory leak and assignment operator for board

--- tests/BoardTest.cpp
+++ tests/BoardTest.cpp
@@ -2,3 +2,17 @@
 // Created by hadar on 27/11/17.
 //
 
+
+#include <gtest/gtest.h>
+GTEST_API_ int main(int argc, char** argv) {
+
+
+    testing::InitGoogleTest(&argc,argv);
+    return RUN_ALL_TESTS();
+}
+
+
+TEST(sdas, sdsd) {
+    int x =1;
+    EXPECT_EQ(1 , x);
+}
