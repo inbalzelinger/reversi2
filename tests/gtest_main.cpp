2f29afc532950a84a2313a38551035167ba56d14
inbalzelinger <inbal.zelinger@gmail.com>
2017-11-28 10:29:23 +0200

    enum

--- tests/gtest_main.cpp
+++ tests/gtest_main.cpp
@@ -5,14 +5,12 @@
 
 #include <gtest/gtest.h>
 GTEST_API_ int main(int argc, char** argv) {
-
-
     testing::InitGoogleTest(&argc,argv);
     return RUN_ALL_TESTS();
 }
 
 
 TEST(sdas, sdsd) {
-    int x =1;
-    EXPECT_EQ(1 , x);
+    int x = 1;
+    EXPECT_EQ(1, x);
 }
---
+++ tests/test_point.cpp
@@ -0,0 +1,16 @@
+//
+// Created by inbal on 27/11/17.
+//
+
+#include <gtest/gtest.h>
+#include "../Point.h"
+
+TEST(PointTest, compareOperator) {
+    Point p1(3 , 4);
+    Point p2(3 , 4);
+    Point p3(5, 6);
+    EXPECT_EQ(p1==p2 , 1);
+    EXPECT_EQ(p1==p3 , 0);
+
+}
+
