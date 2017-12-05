4611d505c36ccb26d8d6bb418c70a65cd292b20b
inbalzelinger <inbal.zelinger@gmail.com>
2017-12-05 15:10:40 +0200

    server start test

---
+++ tests/gtest_server.cpp
@@ -0,0 +1,25 @@
+//
+// Created by inbal on 05/12/17.
+//
+#include <gtest/gtest.h>
+#include "../src/server/Server.h"
+
+#include "iostream"
+using namespace std;
+
+
+TEST(serverTest, start) {
+    int expect = 0;
+    Server s(8000);
+    try {
+        s.start();
+    } catch (const char* msg) {
+        cout<< "cannot start server. Reason: "<< msg << endl;
+        expect = -1;
+    }
+        EXPECT_EQ(expect , 0);
+    }
+
+}
+
+
