b3ffc766cadaf9b6b96367fd13af006a0ec78d84
inbalzelinger <inbal.zelinger@gmail.com>
2017-12-05 16:21:18 +0200

    client

---
+++ Client.cpp
@@ -0,0 +1,50 @@
+//
+// Created by inbal on 05/12/17.
+//
+
+#include "Client.h"
+#include <iostream>
+#include <bits/socket.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <string.h>
+
+using namespace std;
+
+Client::Client(const char *serverIp, int serverPort): serverIP(serverIp) ,
+serverPort(serverPort) , clientSocket(0){
+    cout<< "client c'tor"<<endl;
+}
+
+void Client::connectToServer() {
+    clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket == -1) {
+        throw "Error opening socket";
+    }
+    struct in_addr address;
+    if (!inet_aton(serverIP, &address)) {
+        throw "cant parse IP adress";
+    }
+    struct hostent *server;
+    server = gethostbyaddr((const void *) &address, sizeof(address), AF_INET);
+    if (server == NULL) {
+        throw "Host is unreachable";
+    }
+
+    struct sockaddr_in serverAddress;
+    bzero((char *) &address, sizeof(address));
+
+    serverAddress.sin_family = AF_INET;
+    memcpy((char *) &serverAddress.sin_addr.s_addr, (char *) server->h_addr, server->h_length);
+
+    serverAddress.sin_port = htons(serverPort);
+
+    if (connect(clientSocket, (struct sockaddr *) &serverAddress, sizeof(serverAddress)) == 1) {
+        throw "Error conecting to server";
+    }
+    cout << "connected to server" << endl;
+}
+
+}
---
+++ Client.h
@@ -0,0 +1,22 @@
+//
+// Created by inbal on 05/12/17.
+//
+
+#ifndef REVERSITESTS_CLIENT_H
+#define REVERSITESTS_CLIENT_H
+
+
+class Client {
+public:
+    Client(const char* serverIp , int serverPort);
+    void  connectToServer();
+    int sendMove(int x , int y);
+
+private:
+    const char *serverIP;
+    int serverPort;
+    int clientSocket;
+};
+
+
+#endif //REVERSITESTS_CLIENT_H
